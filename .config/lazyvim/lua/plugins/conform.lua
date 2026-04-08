e87aceb183c194f68087ee6dd0b69b007021718a
dracotato <160530153+DracoTato@users.noreply.github.com>
2026-04-08 20:44:17 +0200

    chore: remove deprecated lazyvim config

--- .config/lazyvim/lua/plugins/conform.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      htmldjango = { "djlint" },
-      python = { "ruff_format", "ruff_organize_imports" },
-    },
-  },
-}
